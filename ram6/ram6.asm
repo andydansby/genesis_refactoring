b57cd1a3337053e52ea26f25cd1f1110943180ff
Andrew Dansby <andydansby@gmail.com>
2019-12-26 04:13:49 -0500

    Add files via upload

--- ram6/ram6.asm
+++ ram6/ram6.asm
@@ -2,6 +2,17 @@
 SECTION BANK_06
 org 0xc000;;believe this is assumed with target ZX
 
+PUBLIC _genesis_title
+_genesis_title:
+	BINARY "genesis_title.bin"
+
+PUBLIC _title_hiscores
+_title_hiscores:
+	BINARY "title_hiscores.bin"
+
+PUBLIC _credits_bkg
+_credits_bkg:
+	BINARY "credits_bkg.bin"
 
 ;;ATTENTION import from object code found in main.c in MAINRAM
 extern _fenemy_defeat
@@ -26,17 +37,7 @@ defc MAX_ENEMIES = 8
 
 
 
-PUBLIC _genesis_title
-_genesis_title:
-	BINARY "genesis_title.bin"
 
-PUBLIC _title_hiscores
-_title_hiscores:
-	BINARY "title_hiscores.bin"
-
-PUBLIC _credits_bkg
-_credits_bkg:
-	BINARY "credits_bkg.bin"
 
 ;;PUBLIC _blackout2
 ;;_blackout2:
