54b7a8dab6e1af58d8b99076a877fa45a3e97f4d
Andrew Dansby <andydansby@gmail.com>
2019-12-31 04:55:55 -0500

    Add files via upload

---
+++ loader/loader.bat
@@ -0,0 +1,24 @@
+cls
+
+cd loading
+	copy loading.scr loadscreen.bin
+	move "loadscreen.bin" "..\"
+cd ..
+
+cd utils
+	copy "bas2tap.exe" "..\"
+	copy "bin2tap.exe" "..\"
+cd ..
+
+	bas2tap -a10 -sbologna loader.bas basloader.tap
+	bin2tap -o loadscreen.tap -a 16384 loadscreen.bin
+
+copy /b basloader.tap + loadscreen.tap loader.tap
+
+
+rem cleanup
+del bas2tap.exe
+del bin2tap.exe
+del loadscreen.bin
+del loadscreen.tap
+del basloader.tap
