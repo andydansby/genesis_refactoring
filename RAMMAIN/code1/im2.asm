368ac367ea9c9b6dd7e5b0964032ac51347a54e9
Andrew Dansby <andydansby@gmail.com>
2020-08-09 07:47:10 -0400

    Add files via upload

--- RAMMAIN/code1/im2.asm
+++ RAMMAIN/code1/im2.asm
@@ -1,11 +1,51 @@
-;;im2.asm
+SECTION ISR_ROUTINE
+
+PUBLIC _IM2ISR
+_IM2ISR:
+	push hl
+	push af
+	push bc
+	push de
+	push ix
+	push iy
+	ex af, af'
+	push af
+	ex af, af'
+
+;//_israddr:
+;//	call 0000 	; to be substituted with the actual ISR handler address
+
+	ex af, af'
+	pop af
+	ex af, af'
+	pop iy
+	pop ix
+	pop de
+	pop bc
+	pop af
+	pop hl
+
+	ei
+ret	; total: 27 bytes!
+
+
+;;---------------------------------
+PUBLIC _Set_screen_flip_flag
+;#BEGIN_ASM
+_Set_screen_flip_flag:
+;; clear the highest bit of the R register. It will be used to flag for a screen switch
+	ld a,r
+	and $7f	
+	ld r,a
+;; set the highest bit of R to 0, so do not switch screen!!!!
+ret
+;#END_ASM
+
+;;---------------------------------
+
 ;;SECTION UNCONTENDED
-EXTERN _borderTest
 
 
-
-
-;---------------------------------
 ;;starts at #615B-6166
 PUBLIC _gameISR1
 ;#BEGIN_ASM
@@ -28,117 +68,11 @@ ret
 
 
 
+;;im2.asm
+;;goes in UNCONTENDED memory
+;;EXTERN _borderTest
+
+;; On the Spectrum 128, interrupt vectors may only be placed between $80ff and $beff inclusive.  An interrupt routing itself must exist wholly between 4000 and $BFFF.  This is because address $c000 to $ffff are pageabe and should an interrupt occur while the wrong 16k RAM is paged in, you will invatiably get a crash.  The ROM area is also constantly paging between the two ROMs so you can't use this as a vector either.  The (new) ROM constantly uses RAM page 7 - the normal page selected by BASIC is RAM page 0.  If an interrupt vector or routine is paged in, the results will be catastrophc.  Be warned.
 
 
-
-
-
-
-
-
-PUBLIC _SetIM2
-;#BEGIN_ASM
-;; Set IM2 mode, prepare IM2 table 
-;;(257 bytes with the same value)
-;; INPUT: 
-;;	A: byte to set for the table -> interrupt will go to AA
-;;	HL: IM2 table (257 bytes, L must be 0)
-;;	DE: address with the ISR handler
-_SetIM2:
-	di
-	ld (israddr + 1), de
-	;; set address in handler
-	;;DE: address with the ISR handler	
-	;;ATTENTION is DEset with correct address on entry?
-	
-	
-	
-	push af
-	ld a, h
-	ld i, a
-	pop af				; set the I register
-
-	ld (hl), a
-	ld d,h
-	ld e,l
-	inc de			
-	ld bc, 256
-	ldir
-	; fill the 257 byte table
-	;; copy data from address HL to address DE with BC used as a counter
-
-	ld d, a
-	ld e, a				; DE=AA		
-	ld hl, BASICISR
-	ld bc, 27
-	ldir; copy BASIC ISR, which will call to the cool one
-;; copy data from address HL to address DE 
-;;with BC used as a counter
-
-	im 2
-	ei
-;;	!!ATTENTION		
-;;halt;;used for tracing program
-
-ret 
-;#END_ASM
-
-PUBLIC BASICISR
-;#BEGIN_ASM
-BASICISR:
-	push hl
-	push af
-	push bc
-	push de
-	push ix
-	push iy
-	ex af, af'
-	push af
-	ex af, af'
-
-israddr:
-	call 0 	; to be substituted with the actual ISR handler address
-
-	ex af, af'
-	pop af
-	ex af, af'
-	pop iy
-	pop ix
-	pop de
-	pop bc
-	pop af
-	pop hl
-
-	ei
-ret	; total: 27 bytes!
-;#END_ASM
-
-;---------------------------------
-
-;;to replace current ISR
-PUBLIC _game_interupt
-;#BEGIN_ASM
-_game_interupt:
-	push hl
-	push af
-	push bc
-	push de
-	push ix
-	push iy
-	ex af, af'
-	push af
-	ex af, af'
-		call _gameISR1
-	ex af, af'
-	pop af
-	ex af, af'
-	pop iy
-	pop ix
-	pop de
-	pop bc
-	pop af
-	pop hl
-	ei
-ret	;
-;#END_ASM
 
